31cfd0c637080ecc283060833272732c5287cbe1
anhha77 <kentucky772001@gmail.com>
2024-02-19 09:37:25 +0700

    update

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -13,28 +13,28 @@ function Card({ data, fromSearch, mediaType }) {
   return (
     <div
       className="movie-card"
-      onClick={() => navigate(`/${data.media_type || mediaType}/${data.id}`)}
+      onClick={() => navigate(`/${data?.media_type || mediaType}/${data?.id}`)}
     >
       <div className="poster-block">
         <Image
           className="poster-img"
           src={`${
-            data.poster_path
-              ? `${IMAGE_URL}/${data.poster_path}`
+            data?.poster_path
+              ? `${IMAGE_URL}/${data?.poster_path}`
               : PosterFallback
           }`}
         />
         {!fromSearch && (
           <>
-            <CircleRating rating={data.vote_average.toFixed(1)} />
-            <Genres data={data.genre_ids.slice(0, 2)} />
+            <CircleRating rating={data.vote_average?.toFixed(1)} />
+            <Genres data={data.genre_ids?.slice(0, 2)} />
           </>
         )}
       </div>
       <div className="text-block">
-        <span className="title">{data.title || data.name}</span>
+        <span className="title">{data?.title || data?.name}</span>
         <span className="date">
-          {dayjs(data.release_date).format("MMM D, YYYY")}
+          {dayjs(data?.release_date).format("MMM D, YYYY")}
         </span>
       </div>
     </div>
